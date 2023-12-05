353da9a6e9e37c3da178c75c64e014b99fba735f
I Wayan Megantara <iwymega@gmail.com>
2023-12-05 14:41:24 +0800

    updateing a note state 1 & 2 and function remove in UI setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,139 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { type } from "os";
+import "./App.css";
+import React, { useState } from 'react';
 
-function App() {
+type Note = {
+  id: number;
+  title: string;
+  content: string;
+}
+
+const App = () => {
+  const [notes, setNotes] = useState<Note[]>([
+    {
+      id: 1,
+      title: "test note 1",
+      content: "bla bla note1",
+    },
+    {
+      id: 2,
+      title: "test note 2 ",
+      content: "bla bla note2",
+    },
+    {
+      id: 3,
+      title: "test note 3",
+      content: "bla bla note3",
+    },
+    {
+      id: 4,
+      title: "test note 4 ",
+      content: "bla bla note4",
+    },
+    {
+      id: 5,
+      title: "test note 5",
+      content: "bla bla note5",
+    },
+    {
+      id: 6,
+      title: "test note 6",
+      content: "bla bla note6",
+    },
+  ]);
+
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const handleAddNote = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log("title: ", title);
+    console.log("content: ", content);
+
+    const newNote: Note = {
+      id: notes.length + 1,
+      title: title,
+      content: content,
+    }
+
+    setNotes([newNote, ...notes]);
+    setTitle("");
+    setContent("");
+  };
+
+  const [selectedNote, setSelectedNote] = useState<Note | null>(null);
+
+  const handleNoteClick = (note: Note) => {
+    setSelectedNote(note);
+    setTitle(note.title);
+    setContent(note.content);
+  };
+
+  const handleUpdateNote = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    if (!selectedNote) {
+      return;
+    }
+
+    const updatedNote: Note = {
+      id: selectedNote.id,
+      title: title,
+      content: content,
+    };
+
+    const updatedNotesList = notes.map((note) => (note.id === selectedNote.id ? updatedNote : note));
+
+    setNotes(updatedNotesList);
+    setTitle("");
+    setContent("");
+    setSelectedNote(null);
+  };
+
+  const handleCancel = () => {
+    setTitle("");
+    setContent("");
+    setSelectedNote(null);
+  };
+
+  const deleteNote = (event: React.MouseEvent, noteId: number) => {
+    event.stopPropagation();
+
+    const updatedNotes = notes.filter((note) => note.id !== noteId);
+
+    setNotes(updatedNotes);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <form onSubmit={(event) => (selectedNote ? handleUpdateNote(event) : handleAddNote(event))} className="note-form">
+        <input value={title} onChange={(event) => setTitle(event.target.value)} placeholder="Title" required />
+        <textarea value={content} onChange={(event) => setContent(event.target.value)} placeholder="Content" rows={10} required></textarea>
+        {selectedNote ? (
+          <div className="edit-buttons">
+            <button type="submit">Save</button>
+            <button onClick={handleCancel}>Cancel</button>
+          </div>
+        ) : (
+          <button type="submit">Add Note</button>
+        )}
+      </form>
+      <div className="notes-grid">
+        {notes.map((note) =>
+          <div key={note.id} className="note-item" onClick={() => handleNoteClick(note)}>
+            <div className="note-header">
+              <button onClick={(event) => deleteNote(event, note.id)}>x</button>
+              <h2>{note.title}</h2>
+              <p>{note.content}</p>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
-}
+};
+
+
+
 
 export default App;
